408d59290759ef8c10574e78f3f9a42ffd23b66d
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-01-31 14:21:46 +0500

    basic_func08.py

--- basic_func08.py
+++ basic_func08.py
@@ -1,5 +1,9 @@
 # Create a function called main.
-
+def main(a):
+    a = a + 1
+    return a
+print(main(int(input("a ="))))
 # Create function arguments a.
-
+    
 # Increase the value of a to one and return.
+
